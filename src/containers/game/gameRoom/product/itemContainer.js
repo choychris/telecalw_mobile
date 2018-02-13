5b66c4a89a5fe499e2cee2d5e82c987907b7553b
benho_kiostech <benho314@gmail.com>
2018-02-13 18:16:42 +0800

    1. Add Image loading buffer

--- src/containers/game/gameRoom/product/itemContainer.js
+++ src/containers/game/gameRoom/product/itemContainer.js
@@ -1,19 +1,33 @@
 import React, { PropTypes, Component } from 'react';
-import { Image , Dimensions } from 'react-native';
+import { Animated , Image , Dimensions } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 class ProductImage extends Component {
+	constructor(props){
+		super(props);
+		this._animation = new Animated.Value(0);
+	}
 	shouldComponentUpdate(){
 		return false;
+	}
+	_startAnimation(){
+		Animated.timing(this._animation,{
+			toValue : 1,
+			duration : 2000
+		}).start();
 	}
 	render(){
 		const { image } = this.props;
 		const { height , width } = Dimensions.get('window');
 		return (image) ? (
-			<Image
+			<Animated.Image
 				source={{ uri : image  }}
-				style={{ width : width*0.8 , height : 180 , marginVertical : 10 }}
+				style={[
+					{ opacity : this._animation },
+					{ width : width*0.8 , height : 180 , marginVertical : 10 }
+				]}
+				onLoad={this._startAnimation()}
 				resizeMode={'contain'}
 			/>
 		) : null
