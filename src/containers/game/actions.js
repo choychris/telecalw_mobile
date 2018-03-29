6c25ada4271a3d378e049f06076fbad468d5e159
benho_kiostech <benho314@gmail.com>
2018-03-29 18:11:09 +0800

    1. Fixed webrtc mechanism to dynamic webrtc server

--- src/containers/game/actions.js
+++ src/containers/game/actions.js
@@ -588,8 +588,8 @@ export function closeAllWebrtc(){
 	return(dispatch,getState)=>{
 		const { webrtcUrl } = getState()['game']['play'];
 		const { front , top } = webrtcUrl;		
-		if(front && front.pc !== undefined) closeWebrtc(front.pc,front.rtsp);
-		if(top && top.pc !== undefined) closeWebrtc(top.pc,top.rtsp);
+		if(front && front.pc !== undefined) closeWebrtc(front.pc,front.rtsp,front.webrtcServer);
+		if(top && top.pc !== undefined) closeWebrtc(top.pc,top.rtsp,top.webrtcServer);
 		dispatch({ type : 'CLEAR_WEBRTC_URL' });
 	}
 }
