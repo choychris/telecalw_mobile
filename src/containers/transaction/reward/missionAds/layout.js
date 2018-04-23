103ed99ecf8fdae738185140c4973a1868d2e29b
Chris Choy <choylapchichris@gmail.com>
2018-04-23 20:42:03 +0800

    finish layout

--- src/containers/transaction/reward/missionAds/layout.js
+++ src/containers/transaction/reward/missionAds/layout.js
@@ -1,11 +1,12 @@
 import React, { PropTypes, Component } from 'react';
 import { ActivityIndicator , ListView , View , Easing,
-         StyleSheet , Text , TouchableOpacity , 
-         Dimensions , Platform, Animated } from 'react-native';
+         StyleSheet , Text , TouchableOpacity ,
+         Dimensions , Platform, Animated, Image } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Telebot from '../../../../components/telebuddies/telebot';
 const height = Dimensions.get('window').height;
+const coinsImg =  require('../../../../../assets/utilities/coins/telecoins_single.png')
 
 class RewardedVideoListContainer extends Component{
   constructor(props){
@@ -20,13 +21,18 @@ class RewardedVideoListContainer extends Component{
   _animate(){
     Animated.timing(this.animatedValue, {
       toValue: 1,
-      duration: 2000,
+      duration: 600,
       easing : Easing.linear
     }).start(() => this._animate())
   }
 
   render(){
     const size = this.animatedValue.interpolate({
+      inputRange: [0, 0.7, 1],
+      outputRange: [0, 1, 0.8]
+    })
+
+    const opacity = this.animatedValue.interpolate({
       inputRange: [0, 1],
       outputRange: [0, 1]
     })
@@ -34,26 +40,63 @@ class RewardedVideoListContainer extends Component{
     return(
       <View style={[styles.container, styles.listWrapper]}>
         <View style={styles.adList}>
-        <Animated.View 
-          style={{ transform: [{scale: size}] }}>
-          <Telebot 
-            status={'money'} 
-            height={height * 0.14} 
-            width={height * 0.14}
-          />
-        </Animated.View>
+          <Animated.View 
+            style={{ transform: [{scale: size}] }}>
+            <Telebot 
+              status={'money'} 
+              height={height * 0.08} 
+              width={height * 0.08}
+            />
+          </Animated.View>
+          <Animated.View style={[styles.itemContainer, {opacity}]}>
+            <View style={{flex: 3}}>
+              <Text style={styles.text}>Complete Watching Video</Text>
+              <View style={styles.coinsRow}>
+                <Image source={coinsImg} style={styles.image} resizeMode={'contain'}/>
+                <Text style={styles.inlineText}>10</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Watch</Text>
+            <Text style={styles.buttonText}>Now</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+        <View style={styles.adList}>
+          <Animated.View 
+            style={{ transform: [{scale: size}] }}>
+            <Telebot 
+              status={'money'} 
+              height={height * 0.08} 
+              width={height * 0.08}
+            />
+          </Animated.View>
+          <Animated.View style={[styles.itemContainer, {opacity}]}>
+            <View style={{flex: 3}}>
+              <Text style={styles.text}>Complete Watching Video</Text>
+              <View style={styles.coinsRow}>
+                <Image source={coinsImg} style={styles.image} resizeMode={'contain'}/>
+                <Text style={styles.inlineText}>10</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Watch</Text>
+            <Text style={styles.buttonText}>Now</Text>
+            </TouchableOpacity>
+          </Animated.View>
         </View>
       </View>
     )
   }
 };
 
+const font = (Platform.OS === 'ios') ? 'Silom' : 'PixelOperator-Bold';
 const styles = StyleSheet.create({
   container : {
     alignSelf : 'stretch',
     alignItems : 'center',
     justifyContent : 'center',
-    paddingVertical : 50
+    paddingVertical : 30
   },
   listWrapper : {
     alignSelf : 'stretch',
@@ -67,6 +110,48 @@ const styles = StyleSheet.create({
     alignSelf : 'stretch',
     padding: 10,
     margin : 5
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection : 'row',
+    alignSelf : 'stretch',
+    alignItems : 'center',
+    borderRadius : 10,
+    backgroundColor : 'black',
+    padding : 10
+  },
+  text : {
+    fontFamily : font,
+    color : '#30D64A',
+    fontSize : 18,
+    marginVertical : 2
+  },
+  image : {
+    width : 28,
+    height : 28,
+    marginRight : 10
+  },
+  coinsRow : {
+    flexDirection : 'row',
+    margin: 0
+  },
+  inlineText: {
+    fontFamily : font,
+    color : '#30D64A',
+    fontSize : 25,
+    marginTop: 6
+  },
+  button: {
+    flex: 1,
+    backgroundColor : '#5AA1AD',
+    borderRadius : 150,
+    alignItems : 'center',
+    paddingVertical: 16
+  },
+  buttonText: {
+    color : 'white',
+    fontFamily : font,
+    fontSize: 14
   }
 })
 
