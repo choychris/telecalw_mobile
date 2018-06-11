4cf91b89e293689c07748ce1bbbd82bd68a11393
ChrisChoy <choylapchichris@gmail.com>
2018-06-11 18:29:21 +0800

    integrate banana game

--- src/containers/game/gamePlayList/miniGamePoster/actions.js
+++ src/containers/game/gamePlayList/miniGamePoster/actions.js
@@ -1,20 +1,20 @@
 import Request from '../../../../utils/fetch';
-let serverlessUrl = "https://miwfhlx21k.execute-api.ap-southeast-1.amazonaws.com/dev/vote/";
 
+const serverlessUrl = 'https://miwfhlx21k.execute-api.ap-southeast-1.amazonaws.com/dev/vote/';
 
-export function toSlides(navigator){
+export function toSlides(navigator) {
   navigator.push({
-    screen : 'app.SlideShow',
-    navigatorStyle : {
-      navBarHidden : true
-    }
+    screen: 'app.SlideShow',
+    navigatorStyle: {
+      navBarHidden: true,
+    },
   });
 }
 
-export function vote(yes){
-  if(yes){
-    Request(serverlessUrl+'yes', 'GET')
-  }else{
-    Request(serverlessUrl+'no', 'GET')
+export function vote(yes) {
+  if (yes) {
+    Request(`${serverlessUrl}yes`, 'GET');
+  } else {
+    Request(`${serverlessUrl}no`, 'GET');
   }
 }
